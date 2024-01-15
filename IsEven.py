7fbd537037772796db3efb6d9ffcf6c95508a9e1
Emil <120837367+EmilProgramer22@users.noreply.github.com>
2024-01-15 13:02:07 +0100

    Initial Commit

---
+++ IsEven.py
@@ -0,0 +1,26 @@
+# First we need to simplify the code
+f = float
+
+# Secound we define the numbers. In this case it's a not even number.
+
+number = f(input("What number? "))
+
+# Now, we divide that number by 2. It is the a variable
+
+a = number / 2
+
+# Now we make an b variable, it's a as an intiger (INT)
+
+b = int(a)
+
+
+# Now we compare them!
+# There is an if and else, if the if (Pun Intended) statment is correct the number is even and if the statment is false the number is not even.
+
+if a == b:
+    print(int(number), "is even")
+else:
+    print(int(number), "isn't even")
+
+
+
